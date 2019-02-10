69a7a550f695c6b8263af6028d61011ca8daa7b8
JCDrapers <47488552+JCDrapers@users.noreply.github.com>
2019-02-10 15:11:19 +0000

    Add files via upload

--- Contact_Book.groovy
+++ Contact_Book.groovy
@@ -1,20 +1,21 @@
 import groovy.swing.SwingBuilder
 import javax.swing.WindowConstants as WC
+import javax.swing.JOptionPane
 import javax.swing.JScrollPane
 import javax.swing.BoxLayout as BXL
 
-def Name = ["James", "Rachel", "Jhon","Jim","Jerry","Barry","Bill","Bob","Ben","Brexit","Bowser","Tara","Angry Cat","Neon Cat","Pepper","Big moomin","Small moomin","My moomin","Rachy","Freddie","Dggo"] as String[]
+def Name = ["James","Jhon","Jim","Jerry","Barry","Bill","Bob","Bill","Brexit","Bowser","Tara","Angry Cat","Neon Cat","Pepper","Big moomin","Small moomin","My moomin","Rachy","Freddie","Dggo"] as String[]
 def arrayLength = (Name.length - 1)
 println(arrayLength)
 int numPanels = arrayLength
 
 swing = new SwingBuilder()
-frame = swing.frame(title:'test', pack:true, visible:true, defaultCloseOperation:WC.HIDE_ON_CLOSE) {
+frame = swing.frame(title:"James' Contact Book", pack:true, visible:true, defaultCloseOperation:WC.HIDE_ON_CLOSE) {
     panel(id:'mainPanel'){
         scrollPane( verticalScrollBarPolicy:JScrollPane.VERTICAL_SCROLLBAR_ALWAYS ) {
             vbox {
-                label("You have $arrayLength contacts")
-                label("Here are their names, press more to see more information")
+                label("    You have $arrayLength contacts    ")
+                label("    Here are their names, press more to see more information     ")
 
                 (0..numPanels).each { num ->
                     def Contact = Name[num]
@@ -25,33 +26,33 @@ frame = swing.frame(title:'test', pack:true, visible:true, defaultCloseOperation
                         button( id: "buttonpanel$num", text:"More", actionPerformed:{
                             frame.setEnabled(false);
                             swing."$panelID".background = java.awt.Color.RED;
-                            frame2 = swing.frame(title:'test', pack:true, visible:true, defaultCloseOperation:WC.DO_NOTHING_ON_CLOSE) {
+                            frame2 = swing.frame(title:"$Contact's Information", pack:true, visible:true, defaultCloseOperation:WC.DO_NOTHING_ON_CLOSE) {
                                 panel(id:'secondPanel') {
                                     vbox {
-                                        label("You have $arrayLength contacts")
+                                        def contact_number = (num+1)
+                                        label("$Contact is contact number  $contact_number , press OK to close this window")
                                         button( id: "buttonpanel$num", text:"OK", actionPerformed: {
                                             frame.setEnabled(true);
                                             frame2.visible = false
                                             swing."$panelID".background = java.awt.Color.LIGHT_GRAY;
                                         }
                                         )
-                                        }
-                                    }
                                     }
                                 }
-                        )
+                            }
                         }
+                        )
                     }
                 }
             }
         }
 
         boxLayout(axis: BXL.Y_AXIS)
-        panel(id:'thirdPanel' , alignmentX: 0f){
+        panel(id:'secondPanel' , alignmentX: 0f){
             button('Quit', actionPerformed:{
                 frame.visible = false
             })
         }
     }
-
+}
 frame.size = [ frame.width, 600 ]
