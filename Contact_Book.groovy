61b8fd0df58ad2293e761eb0bd2e367bcfce99eb
JCDrapers <47488552+JCDrapers@users.noreply.github.com>
2019-02-14 22:44:16 +0000

    Add files via upload
    Big changes to UI:
    Added grid system to align buttons vertically.
    Added Remove button
    Added "Add contact" button
    Added fonts
    Added padding to contact list

--- Contact_Book.groovy
+++ Contact_Book.groovy
@@ -1,48 +1,32 @@
 import groovy.swing.SwingBuilder
-import javax.swing.WindowConstants as WC
+import javax.swing.WindowConstants as Closer
 import javax.swing.JOptionPane
 import javax.swing.JScrollPane
 import javax.swing.BoxLayout as BXL
-import groovy.sql.Sql
-import java.awt.Font
 
-this.getClass().classLoader.rootLoader.addURL(
-        new File("sqlite-jdbc-3.23.1.jar").toURL())
-
-def sql = Sql.newInstance( 'jdbc:sqlite:databasefile.sqlite',
-        'org.sqlite.JDBC' )
-
-//sql.execute """
-//create table file_info (
- // pkey integer primary key,
-  //filename text,
-  //filesize integer
-//)"""
-
-Font title = new Font("Serif", Font.BOLD, 15)
-def Names = ["James","Jhon","Jim","Jerry","Barry","Bill","Bob","Bill","Brexit","Bowser","Tara","Angry Cat","Neon Cat","Pepper","Freddie","Dog","Cat","Tiger"] as String[]
-def arrayLength = (Names.length - 1)
-def display_arraylength = (arrayLength + 1)
+def Name = ["James","Jhon","Jim","Jerry","Barry","Bill","Bob","Bill","Brexit","Bowser","Tara","Angry Cat","Neon Cat","Pepper","Big moomin","Small moomin","My moomin","Rachy","Freddie","Dggo"] as String[]
+def arrayLength = (Name.length - 1)
 println(arrayLength)
 int numPanels = arrayLength
 
 swing = new SwingBuilder()
-frame = swing.frame(title:"James' Contact Book", pack:true, visible:true, defaultCloseOperation:WC.HIDE_ON_CLOSE) {
+frame = swing.frame(title:"James' Contact Book", pack:true, visible:true, defaultCloseOperation:Closer.HIDE_ON_CLOSE) {
     panel(id:'mainPanel'){
         scrollPane( verticalScrollBarPolicy:JScrollPane.VERTICAL_SCROLLBAR_ALWAYS ) {
             vbox {
-                label("    You have $display_arraylength contacts    ").setFont(title)
+                label("    You have $arrayLength contacts    ")
                 label("    Here are their names, press more to see more information     ")
 
                 (0..numPanels).each { num ->
-                    def Contact = Names[num]
+                    def Contact = Name[num]
                     def panelID = "Contact $Contact"
                     def pane = panel( alignmentX:0f, id:panelID, background:java.awt.Color.LIGHT_GRAY ) {
                         label("$Contact")
+//                      textField( id: "description$num", text:panelID, columns: 30 )
                         button( id: "buttonpanel$num", text:"More", actionPerformed:{
                             frame.setEnabled(false);
                             swing."$panelID".background = java.awt.Color.RED;
-                            frame2 = swing.frame(title:"$Contact's Information", pack:true, visible:true, defaultCloseOperation:WC.DO_NOTHING_ON_CLOSE) {
+                            frame2 = swing.frame(title:"$Contact's Information", pack:true, visible:true, defaultCloseOperation:Closer.DO_NOTHING_ON_CLOSE) {
                                 panel(id:'secondPanel') {
                                     vbox {
                                         def contact_number = (num+1)
