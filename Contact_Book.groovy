bb7d505a2aaab909aa1f27346766b296fd5445fa
JCDrapers <47488552+JCDrapers@users.noreply.github.com>
2019-02-15 23:59:24 +0000

    Add files via upload

--- Contact_Book.groovy
+++ Contact_Book.groovy
@@ -36,12 +36,13 @@ def sql = Sql.newInstance("jdbc:sqlite:Contacts.db", "org.sqlite.JDBC")
 
 
 sql.eachRow("select * from Contacts") {
-    println("$it.id and $it.name")
+    //println("$it.id and $it.name")
 }
 
 //Get number of contacts in database
-def row = sql.firstRow("select max(id) as max from Contacts")
-def Number_Of_Contacts = row.max
+def result = sql.firstRow('select count(*) as cont from Contacts')
+long Number_Of_Contacts = result.cont
+ println("$Number_Of_Contacts")
 
 //Fonts
     Font title = new Font("Serif", Font.BOLD, 30)
@@ -63,97 +64,102 @@ def Number_Of_Contacts = row.max
                     label(" You have $Number_Of_Contacts contacts", constraints: "align center, span 6").setFont(title)
                     label("  Press more to see more information").setFont(sub_title)
 
-                    (0..numPanels-1).each { num ->
-                        def panelID = "Contact"
-                        def pane = panel(alignmentX: 0f, id: panelID, background: java.awt.Color.LIGHT_GRAY, layout: new GridLayout(1, 2)) {
-                            sql.eachRow("select * from Contacts WHERE ID = $num+1") {
+                    (0..numPanels).each { num ->
+                        def pane = panel(alignmentX: 0f, background: java.awt.Color.LIGHT_GRAY, layout: new GridLayout(1, 2)) {
+                            sql.eachRow("select rowid, * from Contacts WHERE rowid = $num+1") {
+                                println("$it.rowid")
+                                println("$it.name")
                                 label("   $it.name").setFont(Name)
-                            }
-                            more = button(id: "buttonpanel$num", text: "More", actionPerformed: {
-                                SwingUtilities.updateComponentTreeUI(frame);
-                                swing."$panelID".background = java.awt.Color.RED;
-                                frame.setEnabled(false);
-                                frame2 = swing.frame(title: "$Contact's Information", pack: true, visible: true, defaultCloseOperation: WC.DO_NOTHING_ON_CLOSE) {
-                                    panel(id: 'secondPanel') {
-                                        vbox {
-                                            def contact_number = (num + 1)
-                                            sql.eachRow("select * from Contacts WHERE ID = $num+1") {
-                                                contact_num = num+1
-                                                label("$it.name is contact number $contact_num").setFont(Name)
+                                more = button(id: "buttonpanel$num", text: "More", actionPerformed: {
+                                    frame.setEnabled(false);
+                                    frame2 = swing.frame(title: "Information", pack: true, visible: true, defaultCloseOperation: WC.DO_NOTHING_ON_CLOSE) {
+                                        panel(id: 'secondPanel') {
+                                            vbox {
+                                                //def contact_number = (num + 1)
+                                                sql.eachRow("select rowid, * from Contacts WHERE rowid = $num+1") {
+                                                    contact_num = num+1
+                                                    label("$it.name is contact number $contact_num").setFont(Name)
+                                                }
+                                                ok = button(id: "buttonpanel$num", text: "OK", actionPerformed: {
+                                                    frame.setEnabled(true);
+                                                    frame2.visible = false
+                                                }
+                                                )
+                                                ok.setFocusPainted(false);
+                                                ok.setBackground(new Color(4, 224, 129));
+                                                ok.setFont(Ok_Button)
                                             }
-                                            ok = button(id: "buttonpanel$num", text: "OK", actionPerformed: {
-                                                frame.setEnabled(true);
-                                                frame2.visible = false
-                                                swing."$panelID".background = java.awt.Color.LIGHT_GRAY;
-                                            }
-                                            )
-                                            ok.setFocusPainted(false);
-                                            ok.setBackground(new Color(4, 224, 129));
-                                            ok.setFont(Ok_Button)
                                         }
                                     }
                                 }
-                            }
-                            )
-                            more.setMargin(new Insets(10, 10, 10, 10));
-                            more.setFocusPainted(false);
-                            more.setBackground(new Color(4, 224, 129));
-                            more.setFont(Name)
-                            edit_button = button(id: "buttonpanel$num", text: "Edit", actionPerformed: {
-                                println("text")
+                                )
+                                more.setMargin(new Insets(10, 10, 10, 10));
+                                more.setFocusPainted(false);
+                                more.setBackground(new Color(4, 224, 129));
+                                more.setFont(Name)
 
-                            }
-                            )
+                                edit_button = button(id: "buttonpanel$num", text: "Edit", actionPerformed: {
+                                    println("text")
 
-                            //"Edit" Button Styles
-                            edit_button.setFocusPainted(false);
-                            edit_button.setBackground(new Color(80, 163, 252));
-                            edit_button.setFont(Name)
+                                }
+                                )
+                                //"Edit" Button Styles
+                                edit_button.setFocusPainted(false);
+                                edit_button.setBackground(new Color(80, 163, 252));
+                                edit_button.setFont(Name)
 
-                            remove_button = button(id: "buttonpanel$num", text: "Remove", actionPerformed: {
-                                frame.setEnabled(false)
-                                frame3 = swing.frame(title: "Remove $Contact?", pack: true, visible: true, defaultCloseOperation: WC.DO_NOTHING_ON_CLOSE) {
-                                    panel(id: 'thirdPanel') {
-                                        vbox {
-                                            sql.eachRow("select * from Contacts WHERE ID = $num+1") {
+                                remove_button = button(id: "buttonpanel$num", text: "Remove", actionPerformed: {
+                                    frame.setEnabled(false)
+                                    frame3 = swing.frame(title: "Remove?", pack: true, visible: true, defaultCloseOperation: WC.DO_NOTHING_ON_CLOSE) {
+                                        panel(id: 'thirdPanel') {
+                                            vbox {
+                                                sql.eachRow("select rowid, * from Contacts WHERE rowid = $num+1") {
 
-                                                label("  Are you sure you want to remove $it.name?  ")
+                                                    label("  Are you sure you want to remove $it.name?  ")
 
-                                                remove_confirm = button(id: "buttonpanel$num", text: "Remove $it.name", actionPerformed: {
-                                                    sql.execute("DELETE FROM Contacts WHERE name = $it.name " ) {
-
+                                                    remove_confirm = button(id: "buttonpanel$num", text: "Remove $it.name", actionPerformed: {
+                                                        sql.eachRow("select rowid, * from Contacts WHERE rowid = $num+1") {
+                                                            sql.execute("DELETE FROM Contacts WHERE name = $it.name")
+                                                        }
+                                                        frame.setEnabled(true);
+                                                        frame3.visible = false
                                                     }
+                                                    )
+                                                }
 
+                                                remove_confirm.setFocusPainted(false);
+                                                remove_confirm.setBackground(new Color(200, 4, 21));
+                                                remove_confirm.setFont(Ok_Button)
+
+                                                remove_deny = button(id: "buttonpanel$num", text: "Cancel", actionPerformed: {
                                                     frame.setEnabled(true);
                                                     frame3.visible = false
                                                 }
                                                 )
-                                            }
+                                                remove_deny.setFocusPainted(false);
+                                                remove_deny.setBackground(new Color(4, 224, 129));
+                                                remove_deny.setFont(Ok_Button)
 
-                                            remove_confirm.setFocusPainted(false);
-                                            remove_confirm.setBackground(new Color(200, 4, 21));
-                                            remove_confirm.setFont(Ok_Button)
-
-                                            remove_deny = button(id: "buttonpanel$num", text: "Cancel", actionPerformed: {
-                                                frame.setEnabled(true);
-                                                frame3.visible = false
                                             }
-                                            )
-                                            remove_deny.setFocusPainted(false);
-                                            remove_deny.setBackground(new Color(4, 224, 129));
-                                            remove_deny.setFont(Ok_Button)
 
                                         }
-
                                     }
-                                }
 
+                                }
+                                )
+
+                                //"Remove" Button Styles
+                                remove_button.setFocusPainted(false);
+                                remove_button.setBackground(new Color(200, 4, 21));
+                                remove_button.setFont(Name)
                             }
-                            )
-                            //"Remove" Button Styles
-                            remove_button.setFocusPainted(false);
-                            remove_button.setBackground(new Color(200, 4, 21));
-                            remove_button.setFont(Name)
+
+
+
+
+
+
+
 
                         }
 
