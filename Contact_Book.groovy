f99e0cfd3345ef1afdc8e8b4db7740219ca6ab80
JCDrapers <47488552+JCDrapers@users.noreply.github.com>
2019-02-15 16:38:32 +0000

    Add files via upload
    Removed hard coded array of contacts , added SQLite database instead.
    Selection box around buttons removed.

--- Contact_Book.groovy
+++ Contact_Book.groovy
@@ -1,97 +1,205 @@
+//Import Swing Dependencies
 import groovy.swing.SwingBuilder
 import javax.swing.WindowConstants as WC
 import javax.swing.JOptionPane
 import javax.swing.JScrollPane
 import javax.swing.BoxLayout as BXL
-import groovy.sql.Sql
-import java.awt.Font
-
+import javax.swing.WindowConstants as WC
+import javax.swing.JOptionPane
+import javax.swing.JScrollPane
+import javax.swing.BoxLayout as BXL
 import groovy.swing.SwingBuilder
 import javax.swing.*
 import java.awt.*
 
+//Import SQL Dependencies
+import groovy.sql.Sql
+import java.sql.*
+//import org.sqlite.SQLite
+
+//SQLite force get JDBC file
+@Grapes([
+        @Grab(group='org.xerial',module='sqlite-jdbc',version='3.23.1'),
+        @GrabConfig(systemClassLoader=true)
+])
+
+//Import Font Dependencies
+import java.awt.Font
 
 
+//SQL Setup
+def sql = Sql.newInstance("jdbc:sqlite:Contacts.db", "org.sqlite.JDBC")
+
+//def people = sql.dataSet("Contacts")
+//people.add(id:1, name:"leo")
+//people.add(id:2,name:'yui')
 
 
+sql.eachRow("select * from Contacts") {
+    println("$it.id and $it.name")
+}
 
-
-
-
+//Get number of contacts in database
+def row = sql.firstRow("select max(id) as max from Contacts")
+def Number_Of_Contacts = row.max
 
 //Fonts
-Font title = new Font("Serif", Font.BOLD, 30)
-Font sub_title = new Font("Serif", Font.BOLD, 17)
-Font Name = new Font("Serif", Font.BOLD, 23)
-Font Ok_Button = new Font("Serif", Font.BOLD, 15)
+    Font title = new Font("Serif", Font.BOLD, 30)
+    Font sub_title = new Font("Serif", Font.BOLD, 17)
+    Font Name = new Font("Serif", Font.BOLD, 20)
+    Font Ok_Button = new Font("Serif", Font.BOLD, 15)
+    Font Contact_Add_Button = new Font("Serif", Font.BOLD, 15)
+    Font bottom_buttons = new Font("Serif", Font.BOLD, 14)
 
 
-def Names = ["James","Jhon","Jim","Jerry","Barry","Bill","Bob","Bill","Brexit","Bowser","Tara","Angry Cat","Neon Cat","Pepper","Freddie","Dog","Cat","Tiger","James","Jhon","Jim","Jerry","Barry","Bill","Bob","Bill","Brexit","Bowser","Tara","Angry Cat","Neon Cat","Pepper","Freddie","Dog","Cat","Tiger"] as String[]
-def arrayLength = (Names.length - 1)
-def display_arraylength = (arrayLength + 1)
-println(arrayLength)
-int numPanels = arrayLength
+// Number of Panels to be created from 0
+    int numPanels = Number_Of_Contacts-1
 
-swing = new SwingBuilder()
-frame = swing.frame(title:"James' Contact Book", pack:true, visible:true, defaultCloseOperation:WC.HIDE_ON_CLOSE) {
-    panel(id:'mainPanel'){
-        scrollPane( verticalScrollBarPolicy:JScrollPane.VERTICAL_SCROLLBAR_ALWAYS ) {
-            vbox {
-                label("         You have $display_arraylength contacts    ").setFont(title)
-                label("                  Press more to see more information     ").setFont(sub_title)
+    swing = new SwingBuilder()
+    frame = swing.frame(title: "James' Contact Book", pack: true, visible: true, defaultCloseOperation: WC.HIDE_ON_CLOSE) {
+        panel(id: 'mainPanel') {
+            scrollPane(verticalScrollBarPolicy: JScrollPane.VERTICAL_SCROLLBAR_ALWAYS) {
+                vbox {
+                    label(" You have $Number_Of_Contacts contacts", constraints: "align center, span 6").setFont(title)
+                    label("  Press more to see more information").setFont(sub_title)
 
-                (0..numPanels).each { num ->
-                    def Contact = Names[num]
-                    def panelID = "Contact $Contact"
-                    def pane = panel( alignmentX:0f, id:panelID, background:java.awt.Color.LIGHT_GRAY, layout: new GridLayout(1, 2 )) {
-                        label("   $Contact   ").setFont(Name)
-                        more = button( id: "buttonpanel$num", text:"More", actionPerformed:{
-                            swing."$panelID".background = java.awt.Color.RED;
-                            frame.setEnabled(false);
-                            frame2 = swing.frame(title:"$Contact's Information", pack:true, visible:true, defaultCloseOperation:WC.DO_NOTHING_ON_CLOSE) {
-                                panel(id:'secondPanel') {
-                                    vbox {
-                                        def contact_number = (num+1)
-                                        label("$Contact is contact number  $contact_number , press OK to close this window")
-                                        ok = button( id: "buttonpanel$num", text:"OK", actionPerformed: {
-                                            frame.setEnabled(true);
-                                            frame2.visible = false
-                                            swing."$panelID".background = java.awt.Color.LIGHT_GRAY;
+                    (0..numPanels-1).each { num ->
+                        def panelID = "Contact"
+                        def pane = panel(alignmentX: 0f, id: panelID, background: java.awt.Color.LIGHT_GRAY, layout: new GridLayout(1, 2)) {
+                            sql.eachRow("select * from Contacts WHERE ID = $num+1") {
+                                label("   $it.name").setFont(Name)
+                            }
+                            more = button(id: "buttonpanel$num", text: "More", actionPerformed: {
+                                SwingUtilities.updateComponentTreeUI(frame);
+                                swing."$panelID".background = java.awt.Color.RED;
+                                frame.setEnabled(false);
+                                frame2 = swing.frame(title: "$Contact's Information", pack: true, visible: true, defaultCloseOperation: WC.DO_NOTHING_ON_CLOSE) {
+                                    panel(id: 'secondPanel') {
+                                        vbox {
+                                            def contact_number = (num + 1)
+                                            sql.eachRow("select * from Contacts WHERE ID = $num+1") {
+                                                contact_num = num+1
+                                                label("$it.name is contact number $contact_num").setFont(Name)
+                                            }
+                                            ok = button(id: "buttonpanel$num", text: "OK", actionPerformed: {
+                                                frame.setEnabled(true);
+                                                frame2.visible = false
+                                                swing."$panelID".background = java.awt.Color.LIGHT_GRAY;
+                                            }
+                                            )
+                                            ok.setFocusPainted(false);
+                                            ok.setBackground(new Color(4, 224, 129));
+                                            ok.setFont(Ok_Button)
                                         }
-                                        )
-                                        ok.setBackground(new Color(4, 224, 129));
-                                        ok.setFont(Ok_Button)
                                     }
                                 }
                             }
-                        }
-                        )
-                        more.setBackground(new Color(4, 224, 129));
-                        more.setFont(Name)
-                        remove = button(id: "buttonpanel$num", text:"Remove", actionPerformed:{
-                            println("text")
+                            )
+                            more.setMargin(new Insets(10, 10, 10, 10));
+                            more.setFocusPainted(false);
+                            more.setBackground(new Color(4, 224, 129));
+                            more.setFont(Name)
+                            edit_button = button(id: "buttonpanel$num", text: "Edit", actionPerformed: {
+                                println("text")
+
+                            }
+                            )
+
+                            //"Edit" Button Styles
+                            edit_button.setFocusPainted(false);
+                            edit_button.setBackground(new Color(80, 163, 252));
+                            edit_button.setFont(Name)
+
+                            remove_button = button(id: "buttonpanel$num", text: "Remove", actionPerformed: {
+                                frame.setEnabled(false)
+                                frame3 = swing.frame(title: "Remove $Contact?", pack: true, visible: true, defaultCloseOperation: WC.DO_NOTHING_ON_CLOSE) {
+                                    panel(id: 'thirdPanel') {
+                                        vbox {
+                                            sql.eachRow("select * from Contacts WHERE ID = $num+1") {
+
+                                                label("  Are you sure you want to remove $it.name?  ")
+
+                                                remove_confirm = button(id: "buttonpanel$num", text: "Remove $it.name", actionPerformed: {
+                                                    sql.execute("DELETE FROM Contacts WHERE name = $it.name " ) {
+
+                                                    }
+
+                                                    frame.setEnabled(true);
+                                                    frame3.visible = false
+                                                }
+                                                )
+                                            }
+
+                                            remove_confirm.setFocusPainted(false);
+                                            remove_confirm.setBackground(new Color(200, 4, 21));
+                                            remove_confirm.setFont(Ok_Button)
+
+                                            remove_deny = button(id: "buttonpanel$num", text: "Cancel", actionPerformed: {
+                                                frame.setEnabled(true);
+                                                frame3.visible = false
+                                            }
+                                            )
+                                            remove_deny.setFocusPainted(false);
+                                            remove_deny.setBackground(new Color(4, 224, 129));
+                                            remove_deny.setFont(Ok_Button)
+
+                                        }
+
+                                    }
+                                }
+
+                            }
+                            )
+                            //"Remove" Button Styles
+                            remove_button.setFocusPainted(false);
+                            remove_button.setBackground(new Color(200, 4, 21));
+                            remove_button.setFont(Name)
 
                         }
 
-
-
-                        )
-                        remove.setBackground(new Color(200, 4, 21));
-                        remove.setFont(Name)
-                        }
+                    }
                 }
             }
-        }
 
-        boxLayout(axis: BXL.Y_AXIS)
-        panel(id:'secondPanel' , alignmentX: 0f){
-            button('Quit',constraints : BorderLayout.CENTER, actionPerformed:{
-                frame.visible = false
-            })
-            button('Add Contact',constraints : BorderLayout.CENTER, actionPerformed:{
-                frame.visible = false
-            })
+            boxLayout(axis: BXL.Y_AXIS)
+            panel(id: 'secondPanel', alignmentX: 0f) {
+
+                add_contact = button('Add Contact', constraints: BorderLayout.CENTER, actionPerformed: {
+                    frame.setEnabled(false);
+                    frame4 = swing.frame(title: "hello's Information", pack: true, visible: true, defaultCloseOperation: WC.DO_NOTHING_ON_CLOSE) {
+                        panel(id: 'Add_Contact') {
+                            vbox {
+                                label(" is contact number  , press OK to close this window")
+                                Name_Field = textField(
+                                        columns: 5,
+                                )
+
+                                add_contact_button = button(id: "Contact_added", text: "Add Contact", actionPerformed: {
+                                    frame.setEnabled(true);
+                                    frame4.visible = false
+                                }
+                                )
+                                add_contact_button.setFocusPainted(false);
+                                add_contact_button.setBackground(new Color(4, 224, 129));
+                                add_contact_button.setFont(Contact_Add_Button)
+                            }
+                        }
+                    }
+
+                }
+                )
+
+                quit_button = button('Quit', constraints: BorderLayout.CENTER, actionPerformed: {
+                    frame.visible = false
+                })
+                add_contact.setFocusPainted(false);
+                add_contact.setBackground(new Color(1, 186, 51));
+                add_contact.setFont(bottom_buttons)
+                quit_button.setFocusPainted(false);
+                quit_button.setBackground(new Color(150, 50, 50));
+                quit_button.setFont(bottom_buttons)
+            }
         }
     }
-}
+
 frame.size = [ frame.width, 600 ]
